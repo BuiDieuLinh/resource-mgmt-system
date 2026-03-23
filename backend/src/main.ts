4fc81f67e84ee5a24cbe3ea94500a3331da55c68
BuiDieuLinh <150701836+BuiDieuLinh@users.noreply.github.com>
2026-03-23 10:26:23 +0700

    fix: change BACKEND_PORT to PORT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   });
 
   app.use(cookieParser());
-  const port = process.env.BACKEND_PORT || 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`App running on port ${port}`);
 }
