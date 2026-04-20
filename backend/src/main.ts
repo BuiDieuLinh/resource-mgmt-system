5e51c07781d9cd1993a60640433d3357f571f89c
BuiDieuLinh <buithidieulinh.tlnh@gmail.com>
2026-04-20 15:59:16 +0700

    fix set default ipv4

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
